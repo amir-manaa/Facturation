1747c0e51643c24991aef8d700c708d7ffeb2596
Amir <amir.manaa@gmail.com>
2025-10-22 21:15:04 +0200

    feat: refacto folders

--- apps/frontend/src/app/features/home/home.routes.ts
+++ apps/frontend/src/app/features/home/home.routes.ts
@@ -1,12 +1,12 @@
 import { Route } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { authGuard } from '@guards';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { CustomersComponent } from './customer/customers/customers.component';
-import { ModifyCustomerComponent } from './customer/modify-customer/modify-customer.component';
-import { InvoicesComponent } from './invoice/invoices/invoices.component';
-import { AddCustomerComponent } from './customer/add-customer/add-customer.component';
-import { AddInvoiceComponent } from './invoice/add-invoice/add-invoice.component';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { CustomersComponent } from '../customer/customers/customers.component';
+import { ModifyCustomerComponent } from '../customer/modify-customer/modify-customer.component';
+import { InvoicesComponent } from '../invoice/invoices/invoices.component';
+import { AddCustomerComponent } from '../customer/add-customer/add-customer.component';
+import { AddInvoiceComponent } from '../invoice/add-invoice/add-invoice.component';
 
 export const homeRoutes: Route[] = [
   {
